f418f9293d59c31be6a2208cbca70474605db5ba
Roger Allen <rallen@nvidia.com>
2022-02-03 21:03:23 -0800

    scene reads test1.txt file

--- scene.h
+++ scene.h
@@ -50,6 +50,7 @@ class scene {
   public:
     scene(char *filename)
     {
+        bool got_camera = false;
         std::string line;
         std::ifstream fl(filename);
         if (!fl.good()) {
@@ -59,6 +60,7 @@ class scene {
         }
         while (std::getline(fl, line)) {
             if (line.find("camera") == 0) {
+                got_camera = true;
                 std::istringstream iss(line);
                 std::string camera_str;
                 std::string lfx_str, lfy_str, lfz_str;
@@ -66,7 +68,7 @@ class scene {
                 std::string vux_str, vuy_str, vuz_str;
                 std::string vfo_str, ap_str, foc_str;
                 iss >> camera_str;
-                iss >> lfx_str;  // FIXME try multiple per line
+                iss >> lfx_str; // FIXME try multiple per line
                 iss >> lfy_str;
                 iss >> lfz_str;
                 iss >> lax_str;
@@ -105,10 +107,21 @@ class scene {
                         std::stod(r_str), std::stod(g_str), std::stod(b_str));
                 }
                 else if (type_str == "metal") {
-                    // FIXME
+                    std::string r_str, g_str, b_str, f_str;
+                    iss >> r_str;
+                    iss >> g_str;
+                    iss >> b_str;
+                    iss >> f_str;
+                    new_material->type = METAL;
+                    new_material->metal.albedo = vec3(
+                        std::stod(r_str), std::stod(g_str), std::stod(b_str));
+                    new_material->metal.fuzz = std::stod(f_str);
                 }
                 else if (type_str == "dielectric") {
-                    // FIXME
+                    std::string r_str;
+                    iss >> r_str;
+                    new_material->type = DIELECTRIC;
+                    new_material->dielectric.ref_idx = std::stod(r_str);
                 }
                 else {
                     std::cerr << "ERROR: unknown material type: " << type_str
@@ -119,16 +132,48 @@ class scene {
                     name_str, new_material));
             }
             else if (line.find("sphere") == 0) {
-                std::cout << "!sphere\n";
-                    // FIXME
+                std::istringstream iss(line);
+                std::string sphere_str;
+                std::string cx_str, cy_str, cz_str;
+                std::string r_str;
+                std::string mat_str;
+                iss >> sphere_str;
+                iss >> cx_str;
+                iss >> cy_str;
+                iss >> cz_str;
+                iss >> r_str;
+                iss >> mat_str;
+
+                scene_sphere *new_sphere = new scene_sphere;
+                
+                new_sphere->center = vec3(std::stod(cx_str),std::stod(cy_str),std::stod(cz_str));
+                new_sphere->radius = std::stod(r_str);
+                new_sphere->material_name = mat_str;
+                spheres.push_back(new_sphere);
             }
         }
         fl.close();
+        // check scene
+        if(!got_camera) {
+            std::cerr << "ERROR: Scene did not have a camera." << std::endl;
+            std::exit(4);
+        }
+        if(materials.size() == 0) {
+            std::cerr << "ERROR: Scene did not have any materials." << std::endl;
+            std::exit(4);
+        }
+        if(spheres.size() == 0) {
+            std::cerr << "ERROR: Scene did not have any spheres." << std::endl;
+            std::exit(4);
+        }
+        std::cerr << "read scene file: " << filename << "\n";
+        std::cerr << "material count:  " << materials.size() << "\n";
+        std::cerr << "sphere count:    " << spheres.size() << std::endl;
     }
 
     scene_camera camera;
     std::map<std::string, scene_material *> materials;
-    std::vector<scene_sphere> spheres;
+    std::vector<scene_sphere *> spheres;
 };
 
 #endif
