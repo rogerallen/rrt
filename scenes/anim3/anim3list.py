b9e3372e690ea6f035027777b1dc1df9a415465a
Roger Allen <rallen@gmail.com>
2022-04-30 09:19:57 -0700

    clean up my separate tests/art/creations

--- scenes/anim3/anim3list.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-for i in range(0,200+1,1):
-    print(f"file 'anim3_{i:03d}.png'")
-    print("duration 0.0333333333333333")
-for i in range(199,0,-1):
-    print(f"file 'anim3_{i:03d}.png'")
-    print("duration 0.0333333333333333")
